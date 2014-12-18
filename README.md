aeb89534ebf712fa947888ba27dad5a4d83d0f6c
Kelsey Breseman <ifoundthemeaningoflife@gmail.com>
2014-12-17 22:59:35 -0800

    added use case to readme

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 BLE-web-interface
 =================
 
-Registers nearby BLE devices on a webpage served by a Tessel
+Registers nearby BLE devices on a webpage served by a Tessel.
+
+You have the option to pre-register devices into the code if you want them to show up with a name next to them.
+
+This could be useful if you just want to know what BLE devices are around you– or if a group of people regularly carry BLE-broadcasting devices (e.g. Fitbits or phones), you could register them into the code and use this for a roll-call of who is present.
--- index.js
+++ index.js
@@ -4,12 +4,27 @@ var timeout = 5000;
 var pollFreq = 5000;
 var noneFound;
 var devices = [];
+// Use this to add names to a specific device IDs
+var recognizedDevices = {
+  '105.105.60.123.235.203': 'Myo',
+  '70.10.117.62.49.92': 'August'
+};
 
 // Set up server
 var router = require('tiny-router');
 router.get('/', function (req, res) {
   if (devices.length > 0) {
-    res.send(devices);
+    var page = "<html><body>Devices in view:<br/><br/>";
+    for (var device in devices) {
+      var deviceID = devices[device].id;
+      page += deviceID;
+      if (deviceID in recognizedDevices) {
+        page += "&nbsp;&nbsp;" + recognizedDevices[deviceID];
+      }
+      page += "<br/>";
+    }
+    page += "</body></html>";
+    res.send(page);
   } else {
     res.send('No devices in range.');
   }
